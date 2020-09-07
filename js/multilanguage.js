483c30edaa3fbd634d47378348843bd7654c6408
greg-sid <59452368+greg-sid@users.noreply.github.com>
2020-09-07 15:35:05 +0300

    cv_website

---
+++ js/multilanguage.js
@@ -0,0 +1,214 @@
+var LanguageList = {
+  "EN" : "English (EN)",
+  "GR" : "Ελληνικά (GRE)",
+  "DE" : "Deutsch (DE)"
+};
+
+//languages Objects
+var WORDS_EN = {
+  "about" : "about",
+  "experience" : "projects",
+  "education" : "education",
+  "skills" : "skills",
+  "interests" : "interests",
+  "awards" : "publications",
+  "0" : "Grigoris Sideris",
+  "5pro" : "Projects",
+  "5edu" : "Education",
+  "5skills" : "Skills",
+  "5interests" : "Interests",
+  "5pub" : "Publications & Certifications",
+  "0oop" : "Object Oriented Programming",
+  "0tei" : "Alexander Technological Educational Institute of Thessaloniki",
+  "0uom" : "University of Macedonia",
+  "0prog" : "Programming Languages & Tools",
+  "map-marker" : " Thessaloniki, Greece ",
+  "mobile-phone" : " 6949674060 ",
+  "indoors" : "When indoors:",
+  "outdoors" : "When outdoors:",
+  "indoors1" : "Reading Books",
+  "indoors2" : "Watching Series and Movies",
+  "indoors3" : "Making Puzzles",
+  "indoors4" : "Playing Cards, Board Games and Online PC games",
+  "outdoors1" : "Cinema",
+  "outdoors2" : "Traveling and Exploring new places",
+  "outdoors3" : "Trekking and Hiking",
+  "outdoors4" : "Rafting and Sea Kayak",
+  "about-text" : "Interested in responsive web design and web development with a Master's Degree in Information Systems from University of Macedonia, Greece. Also, highly interested in object-oriented programming and game development.",
+  "3sg" : "Serious Game Development",
+  "serious game" : "Py-rate Adventures is a 2D platfrom game for learning programming concepts in Python. It was implemented in Python using PyGame. Py-rate Adventures is a keyboard action game that will teach to the players the basics of programming using Python language.",
+  "3sgg" : "Serious Game Gallery",
+  "3edu" : "Bachelor degree in Library Science and Information Systems",
+  "3du2" : "Master degree in Information Systems (MIS)",
+  "degree1" : "GPA (Great Point Average): 7.63",
+  "degree2" : "GPA (Great Point Average): 8.71",
+  "primary1" : "October 2011 - November 2015",
+  "primary2" : "February 2017 - March 2019",
+  "primary3" : "February 2019",
+  "seminar1" : "Sideris G., Xinogalos S. PY-RATE ADVENTURES: A 2D platform serious game for learning the basic concepts of programming with Python. Simulation & Gaming, (2019)",
+  "seminar2" : "Certified in advanced programming with Python, 2018",
+  "seminar3" : "Seminar - Google Grow Greek Tourism Online “Web Analytics: Using Data to Help a Business Grow”, 2019",
+  "doilink" : "DOI of the article",
+  "homescreen" : "Home screen of the game",
+  "none" : "Grigoris Sideris"
+  
+  
+};
+
+var WORDS_GR = {
+  "about" : "σχετικα",
+  "experience" : "εργα",
+  "education" : "εκπαιδευση",
+  "skills" : "δεξιοτητες",
+  "interests" : "ενδιαφεροντα",
+  "awards" : "δημοσιευσεις",
+  "0" : "Γρηγορης  Σιδερης",
+  "5pro" : "Εργα",
+  "5edu" : "Εκπαιδευση",
+  "5skills" : "Δεξιοτητες",
+  "5interests" : "Eνδιαφεροντα",
+  "5pub" : "Δημοσιευσεις & Πιστοποιησεις",
+  "0oop" : "Αντικειμενοστρεφης προγραμματισμος",
+  "0tei" : "Αλεξανδρειο Τεχνολογικο Εκπαιδευτικο Ιδρυμα Θεσσαλονικης",
+  "0uom" : "Πανεπιστημιο Μακεδονιας",
+  "0prog" : "Γλωσσες προγραμματισμου & Εργαλεια",
+  "map-marker" : " Θεσσαλονίκη, Ελλάδα ",
+  "mobile-phone" : " 6949674060 ",
+  "indoors" : "Δραστηριότητες στο σπίτι: ",
+  "outdoors" : "Εξωτερικές δραστηριότητες:",
+  "indoors1" : "Λογοτεχνικά - Ιστορικά βιβλία, Μυθιστορήματα",
+  "indoors2" : "Σειρές και ταινίες",
+  "indoors3" : "Συναρμολόγηση παζλ",
+  "indoors4" : "Παιχνίδια με κάρτες, επιτραπέζια και online παιχνίδια",
+  "outdoors1" : "Σινεμά",
+  "outdoors2" : "Ταξίδια",
+  "outdoors3" : "Ορειβασία, πεζοπορία",
+  "outdoors4" : "Rafting και Sea Kayak",
+  "about-text" : "Κάτοχος μεταπτυχιακού διπλώματος στα Πληροφοριακά Συστήματα (Master Information Systems) με ειδικότερη ενασχόληση στον σχεδιασμό και τη δημιουργία ιστοσελίδων (Responsive Web Design and Development). Εξοικειωμένος με τον αντικειμενοστρεφή προγραμματισμό (OOP) για την σχεδίαση και ανάπτυξη προγραμμάτων με ιδιαίτερο ενδιαφέρον στην ανάπτυξη παιχνιδιών.",
+  "3sg" : "Ανάπτυξη παιχνιδιού σοβαρού σκοπού",
+  "serious game" : "Το Py-Rate Adventures είναι ένα παιχνίδι 2D platfrom για την εκμάθηση εννοιών προγραμματισμού στην γλώσσα Python. Υλοποιήθηκε με την Python χρησιμοποιώντας την βιβλιοθήκη PyGame. Το Py-rate Adventures είναι ένα action game που παίζεται με το πληκτρολόγιο και το οποίο βοηθάει τους παίκτες να εξοικειωθούν με βασικές έννοιες του προγραμματισμού χρησιμοποιώντας τη γλώσσα Python.",
+  "3sgg" : "Συλλογή με εικόνες του παιχνιδιού",
+  "3edu" : "Προπτυχιακό δίπλωμα στη Βιβλιοθηκονομία και τα Συστήματα Πληροφόρησης",
+  "3edu2" : "Μεταπτυχιακό δίπλωμα στα Πληροφοριακά Συστήματα (MIS)",
+  "degree1" : "Μ.Ο (Μέσος Όρος): 7.63",
+  "degree2" : "Μ.Ο (Μέσος Όρος): 8.71",
+  "primary1" : "Οκτώβριος 2011 - Νοέμβριος 2015",
+  "primary2" : "Φεβρουάριος 2017 - Μάρτιος 2019",
+  "primary3" : "Φεβρουάριος 2019",
+  "seminar1" : "Sideris G., Xinogalos S. PY-RATE ADVENTURES: A 2D platform serious game for learning the basic concepts of programming with Python. Simulation & Gaming, (2019)",
+  "seminar2" : "Βεβαίωση επιτυχούς παρακολούθησης του μαθήματος:“Προχωρημένος προγραμματισμός με Python” Κέντρο Ανοικτών Διαδικτυακών Μαθημάτων Mathesis Πανεπιστήμιο Κρήτης Ίδρυμα Τεχνολογίας και Έρευνας (2018)",
+  "seminar3" : "“Web Analytics: Using Data to Help a Business Grow”, Google Grow Greek Tourism Online (2019) ",
+  "doilink" : "DOI του άρθρου",
+  "homescreen" : "Αρχική οθόνη του παιχνιδιού",
+  "none" : "Γρηγόρης Σιδέρης"  
+};
+
+var WORDS_DE = {
+  "about" : "über mich",
+  "experience" : "projekte",
+  "education" : "bildung",
+  "skills" : "kompetenzen",
+  "interests" : "Interessen",
+  "awards" : "Veröffentlichungen",
+  "0" : "Grigoris Sideris",
+  "5pro" : "Projekte",
+  "5edu" : "Bildung",
+  "5skills" : "Kompetenzen",
+  "5interests" : "Interessen",
+  "5pub" : "Veröffentlichungen & Zertifizierungen",
+  "0oop" : "Objekt orientierte Programmierung",
+  "0tei" : "Alexander Technological Educational Institute of Thessaloniki",
+  "0uom" : "Universität von Mazedonien",
+  "0prog" : "Programmiersprachen und Tools",
+  "map-marker" : " Thessaloniki, Griechenland ",
+  "mobile-phone" : " 6949674060 ",
+  "indoors" : "Aktivitäten zu Hause:",
+  "outdoors" : "Extern Aktivitäten:",
+  "indoors1" : "Bücher lesen",
+  "indoors2" : "Serien und Filme schauen",
+  "indoors3" : "Rätsel machen",
+  "indoors4" : "Spielkarten, Brettspiele und Online-PC-Spiele",
+  "outdoors1" : "Kino",
+  "outdoors2" : "Reisen und neue Orte erkunden",
+  "outdoors3" : "Trekking und Wandern",
+  "outdoors4" : "Rafting und Seekajak",
+  "about-text" : "Master-Abschluss in Informationssystemen (Master Information Systems) mit einem Fokus auf Responsive Web Design und Entwicklung. Vertraut mit Object Oriented Programming (OOP) für die Gestaltung und Entwicklung von Programmen mit besonderem Interesse an der Spieleentwicklung.",
+  "3sg" : "ernsthafte Spielentwicklung",
+  "serious game" : "Py-rate Adventures ist ein 2D-Plattformspiel zum Erlernen von Programmierkonzepten in Python. Es wurde in Python mit PyGame implementiert. Py-rate Adventures ist ein Keyboard-Action-Spiel, das den Spielern die Grundlagen des Programmierens mit Python vermittelt.",
+  "3sgg" : "Serious Game Gallery",
+  "3edu" : "Bachelor-Abschluss in Bibliothekswesen und Informationssysteme",
+  "3edu2" : "Master in Informationssystemen (MIS)",
+  "degree1" : "Toller Punktedurchschnitt: 7.63",
+  "degree2" : "Toller Punktedurchschnitt: 8.71",
+  "primary1" : "Oktober 2011 - November 2015",
+  "primary2" : "Februar 2017 - März 2019",
+  "primary3" : "Februar 2019",
+  "seminar1" : "PY-RATE ADVENTURES: A 2D Platform Serious Game for Learning the Basic Concepts of Programming With Python. Published on SAGE Journal Simulation & Gaming (September 9, 2019)",
+  "seminar2" : "Zertifiziert für fortgeschrittene Programmierung mit Python, 2018",
+  "seminar3" : "“Web Analytics: Using Data to Help a Business Grow”, Google Grow Greek Tourism Online (Aπρίλιος 2019)",
+  "doilink" : "DOI des Artikels",
+  "homescreen" : "Home screen of the game",
+  "none" : "Grigoris Sideris"
+  
+  
+ 
+ 
+};
+
+
+window.onload = initialize;
+
+function initialize() {
+
+  var $dropdown = $("#country_select");    
+  $.each(LanguageList, function(key, value) {
+    $dropdown.
+      append($("<option/>").
+      val(key).
+      text(value));
+    });
+    
+  loadsLanguage("EN");
+}
+
+function loadsLanguage(lang){
+  /*fills all the span tags with class=lang pattern*/ 
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').each(function(){
+    var LangVar = (this.hash).replace('#','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+  $('h1,h2,h3,[class^="mb"]').each(function(){
+    var LangVar = (this.className).replace('mb-','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+  $('span,li,a[class^="text"]').each(function(){
+    var LangVar = (this.className).replace('text-','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+  $('i[class^="fa fa"]').each(function(){
+    var LangVar = (this.className).replace('fa fa-','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+   $('span[class^="subheading"]').each(function(){
+    var LangVar = (this.className).replace('subheading ','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+    $('span[class^="d-block d-lg"]').each(function(){
+    var LangVar = (this.className).replace('d-block d-lg-','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+   $('li[class^="pub"]').each(function(){
+    var LangVar = (this.className).replace('pub ','');
+    var Text = window["WORDS_"+lang][LangVar];
+    $(this).text(Text);        
+  });
+}
+
+
+
